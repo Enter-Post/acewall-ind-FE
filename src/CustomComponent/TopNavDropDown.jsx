06d04f2ef04da8a88c5484bd1a7c8831cfdef1fd
hamzanaeem03 <hamza04032003naeem@gmail.com>
2025-02-25 04:02:36 +0500

    added icon

--- src/CustomComponent/TopNavDropDown.jsx
+++ src/CustomComponent/TopNavDropDown.jsx
@@ -5,13 +5,21 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "../components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar"; // Use ShadCN's Avatar
+import dummyAvatar from "../assets/avatar.png";
 import { Link } from "react-router-dom";
 
 export function TopNavbarDropDown() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <p className="text-white cursor-pointer">username</p>
+        <div className="flex items-center space-x-2 cursor-pointer">
+          <Avatar className="w-5 h-5">
+            <AvatarImage src={dummyAvatar} alt="User Avatar" />
+            <AvatarFallback>U</AvatarFallback>
+          </Avatar>
+          <p className="text-white">username</p>
+        </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 bg-white">
         <DropdownMenuItem asChild>
